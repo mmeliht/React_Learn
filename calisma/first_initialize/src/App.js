5cb59551eb1e15346d10e12b0d01a1db682f52c6
mmeliht <melihtufekcioglu@gmail.com>
2023-05-03 12:34:38 +0300

    conditional rendering

--- calisma/first_initialize/src/App.js
+++ calisma/first_initialize/src/App.js
@@ -2,8 +2,9 @@ import Header from "./components/header";
 import Body from "./components/body";
 
 
-const name="Melih";
-const surname="Tufekcioglu";
+const name = "Melih";
+const surname = "Tufekcioglu";
+const isLoggedIn = true;
 
 function App() {
   return (
@@ -11,6 +12,11 @@ function App() {
       <Header />
       <Body />
       <p>benim adim {name},soyadim {surname} </p>
+     
+      {isLoggedIn    /* özellikle kelimeleri(if,for,,) kullanmak için {} parantez açmak ve içime yazmak yeterlidir. */
+        ? `Welcome ${name}`
+        : "Bye Bye"
+      }
 
     </div>
   )
